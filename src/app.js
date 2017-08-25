0a730376c31abf7e4ebbb6cd6039fb730614c720
Alexander Anpleenko <vaeum@outlook.com>
2017-08-25 21:14:22 +0300

    add 404 template

--- src/app.js
+++ src/app.js
@@ -15,26 +15,24 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, '../', 'public', 'favicon.ico')));
 app.use(logger('dev'));
+app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, '../', 'public', 'favicon.ico')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/public', express.static(path.join(__dirname, '../public')));
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+  res.render('404', { title: 'Express' });
 });
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
+if (process.env.NODE_ENV == 'development') {
   app.use((err, req, res, next) => {
     res.status(err.code || 500)
       .json({
